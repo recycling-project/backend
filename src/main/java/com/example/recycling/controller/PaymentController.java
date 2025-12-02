7c34c903a46a58609dcd3a5361d5d4196705b574
SY <rsy2169@gmail.com>
2025-12-02 21:24:25 +0900

    임포트 수정

--- src/main/java/com/example/recycling/controller/PaymentController.java
+++ src/main/java/com/example/recycling/controller/PaymentController.java
@@ -1,3 +1,23 @@
+package com.example.recycling.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
